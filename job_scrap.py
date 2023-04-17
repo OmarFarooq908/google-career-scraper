78573d0f89f3369b0baaa40bd3ad39f52d8f6b7f
Muhammad Omar Farooq <mohamed.omar.farooq@gmail.com>
2023-04-18 03:18:39 +0500

    Implemented the SQL saving feature

--- job_scrap.py
+++ job_scrap.py
@@ -1,69 +1,100 @@
+#-----------------------------------------------------------------------------
+#       MIT Licence applies to this file "job_scrap.py"
+#       Author: Muhammad Omar Farooq
+#-----------------------------------------------------------------------------
+
+
 import chromedriver_autoinstaller
 from selenium import webdriver
-import time
 import datetime
-
+import time
 import requests
 from bs4 import BeautifulSoup
 
 
 try:
-    # Your web scraping code here
 
-
-    # Send a GET request to the URL and get the page content
+    # Initialize the selenium chrome driver
     chromedriver_autoinstaller.install()
     driver = webdriver.Chrome()
 
+    # Initialize variables for the website pages
     page = 1
     page_total = 72
     page_loop = page_total + 1
 
+    # Scrape the pages until reached the end of the total pages
     while page != page_loop:
-        print("Should be going good")
+
+        # Load the web page
         driver.get(f'https://careers.google.com/jobs/results/?company=Fitbit&company=GFiber&company=Google&company=YouTube&company=Verily%20Life%20Sciences&company=Waymo&company=Wing&company=X&distance=50&employment_type=FULL_TIME&employment_type=INTERN&hl=en_US&jlo=en_US&page={page}&q=&sort_by=relevance')
+
+        # Wait for the webpage to be completely loaded
         time.sleep(5)
-        print("Tell me")
+
+        # Scrape the html of the web page 
         html = driver.page_source
-        print("Good")
+        
         # Parse the HTML content using Beautiful Soup
         soup = BeautifulSoup(html, 'html.parser')
 
-        # Find all job listings on the page
+        # Find all titles of job posts on the page
         jobs = soup.find_all('h2', {'class':'gc-card__title'})
+
+        # Find all the localities of job posts on the page
         addr_locality = soup.find_all('span', {'itemprop':'addressLocality'})
+
+        # Find all the regions of job posts on the page
         addr_region = soup.find_all('span', {'itemprop':'addressRegion'})
+
+        # Find all the countries of job posts on the page
         addr_country = soup.find_all('span', {'itemprop':'addressCountry'})
+
+        # Find all the qualifications of job posts on the page
         qualifications = soup.find_all('div', {'itemprop':'qualifications'})
+
+        # Find all the time of job posts on the page
         time_posted = soup.find_all('meta', {'itemprop':'datePosted'})
-        print("Not good")
-        # Store the job titles in a text file
+        
+        # Store the scraped job post information in a text file
         with open('google_jobs.txt', 'a') as f:
-            for (title, locality, region, country, qualif, time) in zip(jobs, addr_locality, addr_region, addr_country, qualifications, time_posted):
+            for (title, locality, region, country, qualif, times) in zip(jobs, addr_locality, addr_region, addr_country, qualifications, time_posted):
+
+                # Get the text of the html element containing the title 
                 job_title = title.text
+
+                # Get the text of the html element containing the locality
                 job_locality = locality.txt
+
+                # Get the text of the html element containing the region
                 job_region = region.text
+
+                # Get the text of the html element containing the country
                 job_country = country.text
+
+                # Get the text of the html element containing the qualifications
                 job_qualifications = qualif.text
-                job_time = time['content']
 
-                print("Hey")
+                # Get the text of the html element containing the time
+                job_time = times['content']
 
+
+                # We write the job information into the text file
                 f.write('Title: ' + str(title.get_text()) + '\n')
                 f.write('Address: ' + str(job_locality) + ', ' + str(job_region) + ', ' + str(job_country) + '\n')
                 f.write('Qualifications:\n\t' + str(job_qualifications) + '\n')
                 f.write('Time Posted: ' + str(job_time) + '\n')
                 f.write('\n\n\n\n\n')
 
+
+            # The following string gets written at the end of every page
             f.write('\n\n\n\n\n')
             f.write('----------------------------------------------------\n')
             f.write('End of Page: ' + str(page) + '\t' + str(datetime.datetime.today()) + '\n')
             f.write('----------------------------------------------------\n')
             f.write('\n\n\n\n\n')
 
-        print("Going good")
         page = page + 1
-        print("Still going good?")
 
 except Exception as e:
     print('Error:', e)
--- sql_save.py
+++ sql_save.py
@@ -1,4 +1,17 @@
 import re
+import mysql.connector
+import numpy as np
+
+
+# Change the variables below to suit your MySQL server configurations
+mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "",
+        database = "jobs",
+)
+
+mycursor = mydb.cursor()
 
 # Variable declartions
 titles = []         # Includes titles of all the jobs
@@ -8,6 +21,7 @@ country = []        # Includes countries of all the jobs
 qualifications = [] # Includes qualifications of all the jobs
 time_posted = []    # Includes the time of post of all the jobs
 
+job_all = []
 # Read the file created by jobs_google.py
 f = open('google_jobs.txt', 'r')
 file_content = f.read()
@@ -21,6 +35,15 @@ localities = re.findall(r"(?<=Address:)(.*?)(?=,)", file_content, flags=re.S)
 # Find all the regions between the words 'Address:' and ',,'
 regions = re.findall(r"(?<=Address:)(.*?)(?=,,)", file_content, flags=re.S)
 
+# Find all the countries between the words ',,' and 'Qualifications:'
+country = re.findall(r"(?<=,,)(.*?)(?=Qualifications:)", file_content, flags=re.S)
+
+# Find all the qualifications between the words 'Qualifications:' and 'Time Posted:'
+qualifications = re.findall(r"(?<=Qualifications:)(.*?)(?=Time Posted:)", file_content, flags=re.S)
+
+# Find all the time of posting between the words 'Time Posted:' and 'Title:'
+time_posted = re.findall(r"(?<=Time Posted:)(.*?)(?=\n)", file_content, flags=re.S)
+
 '''Without the use of re
 start = 'Title:'
 end = 'Address:'
@@ -28,7 +51,7 @@ title = file_content[file_content.find(start) + len(start):file_content.rfind(en
 '''
 
 # Cleaning the titles of the new line characters
-if (len(titles)==len(localities)):
+if (len(titles)==len(localities)==len(regions)==len(country)==len(qualifications)==len(time_posted)):
     for i in range(0,len(titles)):
         titles[i] = titles[i].strip()
         localities[i] = localities[i].strip()
@@ -39,6 +62,36 @@ if (len(titles)==len(localities)):
         # Remove the new lines from the splitted string
         regions[i] = regions[i].strip()
 
-print(regions)
-# Printing the titles
-#print(titles)
+        # Remove the new lines from the string
+        country[i] = country[i].strip()
+
+        # Remove all the new lines and tabs in the string
+        qualifications[i] = re.sub('\s+', ' ', qualifications[i])
+        # Remove white spaces from the string
+        qualifications[i] = qualifications[i].strip()
+
+        # Remove all the new lines and tabs in the string
+        time_posted[i] = re.sub('\s+', ' ', time_posted[i])
+        # Remove white spaces from the string
+        time_posted[i] = time_posted[i].strip()
+        # Remove the miliseconds from the string (We have to do that in order to conform to the MySQL 'datetime' data type requirement
+        time_posted[i] = time_posted[i].rsplit('.')[0]
+        # Remove the T from the string and replace it with ' ' (We have to do that in order to conform to the MySQL 'datetime' data type requirement
+        time_posted[i] = time_posted[i].replace('T', ' ')
+
+        temp = []
+
+        temp.extend([titles[i], localities[i], regions[i], country[i], qualifications[i], time_posted[i]])
+        job_all.append(temp)
+
+else:
+    print(" >> There was some 'Error' encountered. Ending the program...........")
+    exit()
+
+sql = "INSERT INTO jobs_google VALUES(%s, %s, %s, %s, %s, %s)"
+val = job_all
+
+mycursor.executemany(sql, val)
+
+mydb.commit()
+print(mycursor.rowcount, "was inserted.")
